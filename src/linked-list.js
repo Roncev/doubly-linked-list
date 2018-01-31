787ae611371f7bb2b8bbc6625c6775055b5d4ba7
moteo <moteoo@gmail.com>
2018-01-31 04:33:18 +0300

    at wrong

--- src/linked-list.js
+++ src/linked-list.js
@@ -10,21 +10,70 @@ class LinkedList {
     }
 
     append(data) {
+      if (this.length === 0){
+      this._head = new Node(data);
+      this._tail = new Node(data);
+    }
+      else if (this.length === 1) {
+        this._tail = new Node(data);
+        this._head.next = this._tail;
+        this.tail.prev = this._head;
+      }
+     else {
+       this._tail.next = new Node(data);
+       this._tail.next.prev = this._tail;
+       this._tail = this._tail.next;
+     }
+     this.length++
+     return this
 
     }
 
     head() {
+      return this._head.data;
     }
 
-    tail() {}
+    tail() {
+      return this._head.data;
+    }
 
-    at(index) {}
+    at(index) {
+      var item = this._head;
+      while (index > 0) {
+        item = item.next;
+        index--;
+      }
+      return item.data
 
-    insertAt(index, data) {}
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        var node = new Node(data);
+        var pos = this.length - index;
+        console.log (pos, index, this.length);
+        node.prev = this._tail;
+        while (pos > 0) {
+          node.prev = node.prev.prev;
+          pos--
+        }
+        node.next = node.prev.next;
+        node.prev.next = node;
+        node.next.prev = node;
+        return this
+    }
 
-    clear() {}
+    isEmpty() {
+      if (this.length === 0 ) {
+        return true
+      }
+      return false
+    }
+
+    clear() {
+      this.length = 0;
+      this._head.data = null;
+      this._tail.data = null;
+    }
 
     deleteAt(index) {}
 
